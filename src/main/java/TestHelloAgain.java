ae92348081df32e3817bc75fe5ed89553e42858e
Kate Li <ekaterina.lizina87@gmail.com>
2022-10-16 00:17:02 -0400

    second commit

--- src/main/java/TestHelloAgain.java
+++ src/main/java/TestHelloAgain.java
@@ -2,8 +2,10 @@ public class TestHelloAgain {
     public static void main(String[] args) {
         System.out.println("Hello Again1");
         System.out.println("Hello Again2");
-
-
+        System.out.println("Hello Again3");
+        System.out.println("Hello Again4");
+        System.out.println("Hello Again5");
+        System.out.println("Hello Again6");
 
     }
 }
